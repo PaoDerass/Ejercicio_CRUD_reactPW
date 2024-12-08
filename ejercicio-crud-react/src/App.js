280383398855824cd0080cc095158dc6602dfdf3
PaoDerass <paola_deras@unitec.edu>
2024-12-08 12:25:43 -0600

    EDICION DE APP.JS

--- ejercicio-crud-react/src/App.js
+++ ejercicio-crud-react/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FormularioUsuario from './components/FormularioUsuario';
+import ListaUsuarios from './components/ListaUsuarios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
+
+
+  const manejarEdicion = (usuario) => {
+    setUsuarioSeleccionado(usuario);
+  };
+
+
+  const manejarGuardado = () => {
+    setUsuarioSeleccionado(null); 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD de Usuarios</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <h3>{usuarioSeleccionado ? 'Editar Usuario' : 'Crear Usuario'}</h3>
+          <FormularioUsuario
+            usuarioSeleccionado={usuarioSeleccionado}
+            alGuardar={manejarGuardado}
+          />
+        </div>
+        <div className="col-md-8">
+          <h3>Lista de Usuarios</h3>
+          <ListaUsuarios alEditar={manejarEdicion} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
